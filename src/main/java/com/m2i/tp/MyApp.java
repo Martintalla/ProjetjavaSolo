22765633cd5a0661367dd195e0178bd0b09fb25d
MartinTALLA <tallamartin@rocketmail.com>
2023-06-12 14:02:40 +0200

    nouvelle modif

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,7 +5,8 @@ public class MyApp {
 	public static void main(String[] args) {
 
 		System.out.println("lund 12 juin 2023 12h14");
-
+                System.out.println("Bientôt la pause déjeuner");
+	        System.out.println("Bientôt la fin de la journ�e");
 
 	}
 
