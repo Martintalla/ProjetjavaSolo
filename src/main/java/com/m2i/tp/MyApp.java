961749ee2e3408e6f12016a53f877b209cf1581a
MartinTALLA <tallamartin@rocketmail.com>
2023-06-12 12:20:06 +0200

    modif2

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -3,7 +3,7 @@ package com.m2i.tp;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("lund 12 juin 2023");
+		System.out.println("lund 12 juin 2023 12h14");
 
 	}
 
